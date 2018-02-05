e99cc68164a914e7a776ff6e6f860f7ba266d6a9
kseebrinegar <ksee.brinegar@hotmail.com>
2018-02-05 13:45:18 -0800

    new

--- index.js
+++ index.js
@@ -36,14 +36,14 @@ require('./routes/logout')(app);
 require('./routes/stripe')(app);
 require('./routes/getOrders')(app);
 
-if (process.env.NODE_ENV === 'production') {
+/*if (process.env.NODE_ENV === 'production') {
 	// Express will serve up production assets
 	app.use(express.static('client/build'));
 
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 	})
-}
+}*/
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
